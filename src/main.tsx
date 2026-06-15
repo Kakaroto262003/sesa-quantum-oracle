beb1acfeea20a20212db2c8d5f71c92a2eacbce2
Kakaroto262003 <sesariuspawika910@gmail.com>
2026-06-15 21:31:49 +0800

    fix: resolve mapbox css layout container visibility

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,13 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.tsx';
+import './index.css';
 
-// Menggunakan ReactDOM (huruf DOM besar) sesuai dengan pemanggilan di baris ke-2
+// INJEKSI STYLESHEET SAKRAL MAPBOX UNTUK MEMAKSA LAYOUT 3D GLOBE MUNCUL
+import 'mapbox-gl/dist/mapbox-gl.css';
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-)
+);
